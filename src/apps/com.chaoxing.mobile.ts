f08695f83d413d8e32a3d9a45b7e219b3ac7e4e1
Lisrain <89452996+lisrain@users.noreply.github.com>
2025-11-29 14:55:16 +0800

    Create com.chaoxing.mobile.ts

---
+++ src/apps/com.chaoxing.mobile.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+      "id": "com.chaoxing.mobile",
+      "name": "学习通",
+      "groups": [
+        {
+          "key": 1,
+          "name": "功能类-学习通电脑端自动登录",
+          "desc": "学习通电脑端自动登录",
+          "rules": [
+            {
+              "action": "clickCenter",
+              "matches": [
+                "@[desc=\"登录\"][visibleToUser=true] < View <2 [text=\"登录确认\"]"
+              ],
+              "activityIds": [
+                "com.chaoxing.mobile.webapp.ui.WebAppViewerActivity"
+              ]
+            }
+          ]
+        }
+      ]
+});

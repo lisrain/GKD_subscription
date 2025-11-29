1e012e13d031e44f4149438f48d2ccc5061170df
Lisrain <89452996+lisrain@users.noreply.github.com>
2025-11-29 14:19:03 +0800

    Update apps subs

---
+++ src/apps/com.MobileTicket.ts
@@ -0,0 +1,24 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+     "id": "com.MobileTicket",
+      "name": "铁路12306",
+      "groups": [
+        {
+          "key": 1,
+          "name": "跳过学生票弹窗",
+          "desc": "自动点击跳过按钮跳过学生票弹窗",
+          "rules": [
+            {
+              "matches": [
+                "[vid=\"cancel\"]"
+              ],
+              "fastQuery": true,
+              "activityIds": [
+                "com.MobileTicket.ui.activity.MainActivity"
+              ]
+            }
+          ]
+        }
+      ]
+});

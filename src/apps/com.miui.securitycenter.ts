e454e873e869368af1fab24dc32f1774a0e877af
Lisrain <89452996+lisrain@users.noreply.github.com>
2025-11-29 14:49:40 +0800

    Add security center app configuration

---
+++ src/apps/com.miui.securitycenter.ts
@@ -0,0 +1,37 @@
+import { defineGkdApp } from '@gkd-kit/define';
+
+export default defineGkdApp({
+   {
+      "id": "com.miui.securitycenter",
+      "name": "安全服务",
+      "groups": [
+        {
+          "key": 16,
+          "name": "[弹窗类]自动关闭不足xx分体验优化",
+          "desc": "自动关闭不足xx分体验优化弹窗",
+          "rules": [
+            {
+              "key": 1,
+              "matches": [
+                "[text=\"体检优化\"] < [vid=\"topPanel\"] + * + [vid=\"buttonPanel\"] > [text=\"退出\"]"
+              ],
+              "fastQuery": true,
+              "activityIds": [
+                "com.miui.securityscan.MainEntryActivity"
+              ]
+            },
+            {
+              "key": 2,
+              "matches": [
+                "[text=\"体检优化\"] < [vid=\"topPanel\"] + * + [vid=\"buttonPanel\"] > [text=\"取消\"]"
+              ],
+              "fastQuery": true,
+              "activityIds": [
+                "com.miui.securityscan.MainEntryActivity"
+              ]
+            }
+          ]
+        }
+      ]
+    },
+});

c3c2decb699a3a74902ecfd23a2387f016f7166b
lisrain <liangcheng1130@163.com>
2025-12-23 16:36:21 +0800

    feat: 完善 铁路12306 规则

--- src/apps/com.MobileTicket.ts
+++ src/apps/com.MobileTicket.ts
@@ -18,12 +18,30 @@ export default defineGkdApp({
     },
     {
       key: 2,
-      name: '功能类-自动确认登录',
-      desc: '自动确认登录12306网站',
+      name: '功能类-自动确认登录网站',
+      desc: '自动确认登录12306网站&本人车票校验',
       rules: [
         {
+          key: 1,
+          fastQuery: true,
           activityIds: 'com.alipay.mobile.nebulacore.ui.H5Activity',
-          matches: '@[text="确认登录"] -2 [text="12306网站登录确认"]',
+          matches:
+            '@[text="确认登录"][visibleToUser=true] -2 [text="12306网站登录确认"]',
+        },
+        {
+          key: 2,
+          preKeys: [1],
+          fastQuery: true,
+          activityIds: 'com.alipay.mobile.nebulacore.ui.H5Activity',
+          matches:
+            '@[vid="sure"][visibleToUser=true] < LinearLayout - FrameLayout > [text="登录成功"]',
+        },
+        {
+          key: 3,
+          fastQuery: true,
+          activityIds: 'com.alipay.mobile.nebulacore.ui.H5Activity',
+          matches:
+            '@[vid="sure"][visibleToUser=true] < LinearLayout - FrameLayout > [text="身份核验成功！"]',
         },
       ],
     },
